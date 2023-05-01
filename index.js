d28350427d69c0104d4cf3a3667babf96f4f0579
HumbertL <nerdinvader67@gmail.com>
2023-05-01 13:18:09 -0600

    completado de endpoint de rubricas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const rubricaRoute = require("./routes/rubricas-routes");
 
 const app = express();
 const port = 3000;
@@ -51,4 +52,7 @@ app.use(
   express.static(path.join(__dirname, "/public/html/evaluar"))
 );
 
+app.use("/rubricas", express.static(path.join(__dirname, "/public/html/rubricas")));
+
+app.use("/api/rubricas", rubricaRoute);
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
